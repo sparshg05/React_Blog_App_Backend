d07e0ccbaf9a71c9fc6556665d888997c266dca8
Sparsh Goyal <sparshg05@gmail.com>
2022-01-19 18:27:49 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverless = require("serverless-http");
+
+const app = express();
+const cors = require("cors");
+const data = require('./course.json');
+const PORT=8080;
+
+const router = express.Router();
+
+app.use(cors());
+app.use(`/.netlify/functions/api`, router);
+
+app.get("/jasonData" , (req,res)=>{
+    // console.log(req.query);
+    res.send(data);
+})
+
+app.listen(PORT , ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+});
+module.exports.handler = serverless(app);
